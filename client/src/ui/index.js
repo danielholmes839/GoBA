4ae66da5baf05ff0ef8bbaa4e5976f79babb89a0
Daniel <danielh839@gmail.com>
2021-10-23 15:09:45 -0400

    New build

--- client/src/ui/index.js
+++ client/src/ui/index.js
@@ -1,105 +1,101 @@
-import Vue from 'vue';
-import axios from 'axios';
+import Vue from "vue";
+import axios from "axios";
 import { setup } from "../game";
 
-const host = "goba-env.eba-hiw6diij.ca-central-1.elasticbeanstalk.com"; // "localhost:5000";
+const host = "goba.holmes-dev.com"; // "localhost:5000";
 console.log(host);
 
 let app = new Vue({
-    el: "#app",
-    data: {
-        inGame: false,
+  el: "#app",
+  data: {
+    inGame: false,
 
-        scores: [],
+    scores: [],
 
-        code: "",
+    code: "",
 
-        createName: "",
-        createError: "",
-        createErrorMessage: "",
+    createName: "",
+    createError: "",
+    createErrorMessage: "",
 
-        joinName: "",
-        joinError: false,
-        joinErrorMessage: false,
+    joinName: "",
+    joinError: false,
+    joinErrorMessage: false,
+  },
+
+  methods: {
+    updateTPS: function(message) {
+      if (message !== this.tps) {
+        this.tps = message;
+      }
     },
 
-    methods: {
-        updateTPS: function (message) {
-            if (message !== this.tps) {
-                this.tps = message;
-            }
-        },
+    updateScores: function(scores) {
+      let formatted = [];
 
-        updateScores: function (scores) {
-            let formatted = [];
+      for (let [name, score] of Object.entries(scores)) {
+        score.name = name;
+        formatted.push(score);
+      }
+      formatted = formatted.sort((a, b) => {
+        return a.kills < b.kills ? 1 : -1;
+      });
+      this.scores = formatted;
+    },
 
-            for (let [name, score] of Object.entries(scores)) {
-                score.name = name;
-                formatted.push(score);
-            }
-            formatted = formatted.sort((a, b) => {
-                return (a.kills < b.kills) ? 1 : -1;
-            });
-            this.scores = formatted;
-        },
+    createGame: function() {
+      let name = this.createName;
 
-        createGame: function () {
-            let name = this.createName;
+      axios.get(`https://${host}/create?name=${name}`).then((result) => {
+        this.code = result.data.code;
 
-            axios.get(`http://${host}/create?name=${name}`).then(result => {
-                this.code = result.data.code;
-
-                if (result.data.success) {
-                    this.createGameJoin(this.code, name);
-                } else {
-                    this.createError = true;
-                    this.createErrorMessage = result.data.error;
-                }
-            });
-        },
-
-        createGameJoin: function (code, name) {
-            let url = `ws://${host}/join?code=${code}&name=${name}`;
-            let socket = new WebSocket(url);
-
-            socket.onmessage = (message) => {
-                let event = JSON.parse(message.data);
-
-                if (event.data.success) {
-                    this.inGame = true;
-                    setup(socket, app);
-                }
-
-                else {
-                    this.createError = true;
-                    this.createErrorMessage = event.data.error;
-                    socket.close();
-                }
-            }
-        },
-
-        joinGame: function () {
-            let url = `ws://${host}/join?code=${this.code}&name=${this.joinName}`;
-            let socket = new WebSocket(url);
-
-            socket.onmessage = (message) => {
-                let event = JSON.parse(message.data);
-
-                if (event.data.success) {
-                    this.inGame = true;
-                    setup(socket, app);
-                }
-
-                else {
-                    this.joinError = true;
-                    this.joinErrorMessage = event.data.error;
-                    socket.close();
-                }
-            }
-        },
-
-        refresh: function () {
-            location.reload();
+        if (result.data.success) {
+          this.createGameJoin(this.code, name);
+        } else {
+          this.createError = true;
+          this.createErrorMessage = result.data.error;
         }
-    }
+      });
+    },
+
+    createGameJoin: function(code, name) {
+      let url = `wss://${host}/join?code=${code}&name=${name}`;
+      let socket = new WebSocket(url);
+
+      socket.onmessage = (message) => {
+        let event = JSON.parse(message.data);
+
+        if (event.data.success) {
+          this.inGame = true;
+          setup(socket, app);
+        } else {
+          this.createError = true;
+          this.createErrorMessage = event.data.error;
+          socket.close();
+        }
+      };
+    },
+
+    joinGame: function() {
+      let url = `wss://${host}/join?code=${this.code}&name=${this.joinName}`;
+      let socket = new WebSocket(url);
+
+      socket.onmessage = (message) => {
+        let event = JSON.parse(message.data);
+
+        if (event.data.success) {
+          this.inGame = true;
+          setup(socket, app);
+        } else {
+          this.joinError = true;
+          this.joinErrorMessage = event.data.error;
+          socket.close();
+        }
+      };
+    },
+
+    refresh: function() {
+      location.reload();
+    },
+  },
 });
